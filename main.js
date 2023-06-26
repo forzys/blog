eea843b4dbb8e3846484de792532d28d3f268062
zys <zys@outlook.in>
2023-06-26 13:23:13 +0800

    update main.js

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-// most @actions toolkit packages have async methods
-async function run() {
+ 
+async function main() {
     try { 
         var owner = process.env['GITHUB_NAME']
         var repo = process.env['GITHUB_REPO']
@@ -15,11 +14,11 @@ async function run() {
         core.info(`ISSUES_DIST = '${dist}'`);
 
         let ghToken = process.env['GITHUB_TOKEN']
-      
-
+       
         if (!ghToken) {
             throw new Error('GITHUB_TOKEN not defined')
         }
+        
         let ghWorkspacePath = process.env['GITHUB_WORKSPACE']
         if (!ghWorkspacePath) {
             throw new Error('GITHUB_WORKSPACE not defined')
@@ -33,21 +32,42 @@ async function run() {
         }
 
         const octokit = github.getOctokit(ghToken);
-        const { data: issues } = await octokit.issues.listForRepo({
-            owner,
-            repo,
-        });
+        const { data } = await octokit.issues.listForRepo({ owner, repo }); 
+ 
+        let issues = []
+        for(let i= 0; i< data.length; i+= 15){
+            issues.push(data.slice(i, i + 15)) 
+        }
+ 
+ 
+        fs.writeFile(path.join(issuesDir, 'blog.json'), JSON.stringify({ total: data.length, pageSize: 15 }), (err)=>{
+            if (err) { throw err } 
+        }); 
 
-        issues.forEach(ele => {
-            let labels = ele.labels.map(label => { return label.name })
-            let header = `---\ntitle: "${ele.title}"\nauthor: "${ele.user.login}"\nlabels: [${labels}]\ndate: ${ele.created_at}\neditLink: false\n---\n`
-            let file = path.join(issuesDir, ele.number + '.md');
-            fs.writeFile(file, header + ele.body, (err) => {
-                if (err) {
-                    throw err
-                }
 
-                core.info(`issue#${ele.number} -> ${file}`)
+        issues.forEach((issue, index)=>{
+            const info = { page: index + 1, total: data.length, pageSize: 15, data: [] }
+            const name = 'blog_' + index + '.json'
+           
+            issue.forEach(ele=>{
+                let file = path.join(issuesDir, ele.number + '-' + ele.title.replace(' ','_') + '.md');
+ 
+                info.data.push({
+                    title: ele?.title,
+                    author:ele?.user?.login,
+                    labels:ele?.labels?.map(i=>i?.name),
+                    updated:ele?.created_at,
+                    name : ele?.number + '-' + ele?.title.replace(' ','_') + '.md'
+                });
+
+                fs.writeFile(file, ele.body, (err) => {
+                    if (err) { throw err }
+                    core.info(`issue#${ele.number} -> ${file}`)
+                });
+            });
+
+            fs.writeFile(name,  JSON.stringify(info), (err) => {
+                if (err) { throw err }
             });
         });
 
@@ -56,4 +76,4 @@ async function run() {
     }
 }
 
-run()
+main()
