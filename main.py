59b783a7002d2d8526174aa5b2d26c5c1c6fbfd3
zys <zys@outlook.in>
2023-06-28 23:38:04 +0800

    update backup folder

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ My personal blog using issues and GitHub Actions (随意转载，无需署名)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
-BACKUP_DIR = "BACKUP"
+BACKUP_DIR = "backup"
 ANCHOR_NUMBER = 5
 TOP_ISSUES_LABELS = ["Top"]
 TODO_ISSUES_LABELS = ["TODO"]
