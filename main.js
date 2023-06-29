2a2582d94f8d53ea76f4ef9e646ae1e45734ca60
zys <zys@outlook.in>
2023-06-29 13:08:15 +0800

    update main.js

--- main.js
+++ main.js
@@ -21,8 +21,7 @@ function onArchive(issues, space){
         fs.mkdirSync(externalDir)
     }
     
-    const files = fs.readdirSync(externalDir); 
-
+    const files = fs.readdirSync(externalDir);  
     const exts = files.map(name=>{
         const body = fs.readFileSync(externalDir + '/' + name).toString();
         const [, config] = body?.match(new RegExp('<!-- config: (.*?) -->')) || []
@@ -39,8 +38,6 @@ function onArchive(issues, space){
         return b?.created_at - a?.created_at
     });
 }
- 
-
 
 async function main() {
     try { 
@@ -73,49 +70,44 @@ async function main() {
 
         const octokit = github.getOctokit(ghToken);
         const list = await octokit.issues.listForRepo({ owner, repo }); 
-     
-        const values = onArchive(list?.data, ghWorkspacePath)
-
-        let issues = []
-        for(let i= 0; i< values?.length; i+= 15){
-            issues.push(values?.slice(i, i + 15)) 
-        }
+  
+        // join External folder
+        const issues = onArchive(list?.data, ghWorkspacePath)
  
-        fs.writeFile(path.join(issuesDir, 'blog.json'), JSON.stringify({ total: values.length, pageSize: 15 }), (err)=>{
-            if (err) { throw err } 
-        });
+        const info = { total: issues.length, data: [] }
 
-        issues.forEach((issue, index)=>{
-            const info = { page: index + 1, total: values.length, pageSize: 15, data: [] }
-            const name = 'blog_' + index + '.json'
+        /**
+         * 循环获取Issues
+         * 1. 写入 md 文件
+         * 2. 写入索引文件
+         */
+        issues.forEach(ele=>{ 
+            let title_name = [ele?.number, ele?.title.replace(/ /g,'_')].filter(Boolean).join('-')
+            let file = path.join(issuesDir, title_name + '.md');
             
-            issue.forEach(ele=>{ 
-                let title_name = [ele?.number, ele?.title.replace(/ /g,'_')].filter(Boolean).join('-')
-                let file = path.join(issuesDir, title_name + '.md');
-                const [, intro] = ele?.body?.match(new RegExp('<!-- intro: (.*?) -->')) || []
+            // 获取 markdown 文件 intro
+            const [, intro] = ele?.body?.match(new RegExp('<!-- intro: (.*?) -->')) || []
 
-                info.data.push({
-                    id: ele?.id,
-                    title: ele?.title,
-                    author:ele?.user?.login || owner,
-                    labels:ele?.labels?.map(i=>i?.name),
-                    updated:ele?.created_at,
-                    name : title_name + '.md',
-                    intro: intro, 
-                    issues_url: ele?.html_url,
-                });
-
-                fs.writeFile(file, ele.body, (err) => {
-                    if (err) { throw err }
-                    core.info(`issue#${ele.number} -> ${file}`)
-                });
+            info.data.push({
+                id:     ele?.id,
+                title:  ele?.title,
+                author: ele?.user?.login || owner,
+                labels: ele?.labels?.map(i=>i?.name),
+                updated:ele?.created_at,
+                name :  title_name + '.md',
+                intro:  intro, 
+                issues_url: ele?.html_url,
             });
 
-            fs.writeFile(path.join(issuesDir, name),  JSON.stringify(info), (err) => {
+            fs.writeFile(file, ele.body, (err) => {
                 if (err) { throw err }
+                core.info(`issue#${ele.number} -> ${file}`)
             });
         });
 
+        fs.writeFile(path?.join(issuesDir, 'blog.json'),  JSON.stringify(info), (err) => {
+            if (err) { throw err }
+        })
     } catch (error) {
         core.setFailed(error.message);
     }
