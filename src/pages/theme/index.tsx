7096991e6e76be49b209c05cfc2414e733f6bc2b
forzys <zys@outlook.in>
2023-07-01 20:38:27 +0800

    update theme

--- src/pages/theme/index.tsx
+++ src/pages/theme/index.tsx
@@ -1,16 +1,14 @@
  
 import { memo, useCallback, useEffect, useLayoutEffect } from "react"; 
 import Input from '@/components/input'
-import Button from '@/components/button'
-import { Search } from '@/components/icons' 
-import Stretch from '@/components/stretch' 
+import Button from '@/components/button' 
 import Spining from '@/components/spining'  
 import { useMemoizedFn, useFullScreen, useCreation, usePagination, useUpdate } from '@/common/hooks' 
 import { useFetch, apis } from '@/request/index'
 import Group from "@/components/group";
 import Tooltip from '@/components/tooltip'
 import Card from "@/components/card";
-import './index.css'
+import styles from './index.module.css'
 import Segment from "@/components/segment";
 import Pagination from '@/components/pagination'
 
@@ -70,52 +68,62 @@ export default memo((props)=>{
         onFetchThemes(type, 0)
     });
 
+    const onUseColor:any = useMemoizedFn((info:any[])=>{
+     
+        const body = document.body;
+
+        // const colors = ['--bg-0', '--bg-30', '--bg-60', '--bg-100']
+ 
+        const [color0, color30, color60, color100] = info
+ 
+        body.style.setProperty('--bg-0', color0);
+        body.style.setProperty('--bg-30', color30);
+        body.style.setProperty('--bg-60', color60);
+        body.style.setProperty('--bg-100', color100);
+    })
  
  
     return (
         <div className="main">
-            <Card 
-                bodyClassName="themes-box" 
-                headerStyle={{paddingBottom:6}} 
-                bodyStyle={{ paddingBottom: 0, paddingTop: 0}} 
-                title="主题色搭配"
-            >
-                <Segment 
-                    fontSize="0.5rem"
-                    onChange={onTypeChange}
-                    options={[ 
-                        {label: 'New', value: 'new'},
-                        {label: 'Popular', value: 'popular'},
-                    ]}
-                    className="themes-segment"
-                />
+            <Segment 
+                fontSize="0.5rem"
+                onChange={onTypeChange}
+                options={[ 
+                    {label: 'New', value: 'new'},
+                    {label: 'Popular', value: 'popular'},
+                ]} 
+                className={styles.segment}
 
-                <Spining loading={state?.loading} style={{flex: 1}}> 
-                    <div className="themes">
-                        {
-                            state?.themes?.map((item:any)=>{
-                                const paint = paintPalette(item.code)
-                                return (
-                                    <div key={item.code} className="theme">
-                                        {paint.map((color:string)=>{
-                                            return (
-                                                <div key={color} className="item" style={{ background:color, '--color': color } as any}>
-                                                    <span>{color}</span>
-                                                </div>
-                                            )
-                                        })}
-                                    </div>
-                                )
-                            })
-                        } 
-                    </div>
-                </Spining>
-                 
-                <div className="themes-footer">
-                    <Pagination total={13} page={Number(state.page) + 1} onChange={onPageChange} />
-                </div> 
-            </Card> 
+            />
 
+            <Spining loading={state?.loading} style={{ flex: 1}}> 
+                <div className={styles.themes}>
+                    {
+                        state?.themes?.map((item:any)=>{
+                            const paint = paintPalette(item.code)
+                            return (
+                                <div key={item.code} className={styles.theme}>
+                                    {paint.map((color:string, index:number)=>{
+                                        return (
+                                            <div key={color} className={styles.item +' ' + styles['color' + index]} style={{ background:color, '--color': color } as any}>
+                                                <span>{color}</span> 
+                                            </div>
+                                        )
+                                    })}
+                                    <Tooltip label="再次点击切换顺序">
+                                        <span className={styles.usefull} onClick={onUseColor.bind(null,paint)}>Use</span>
+                                    </Tooltip>
+                                    
+                                </div>
+                            )
+                        })
+                    } 
+                </div>
+            </Spining>
+                
+            <div  className={styles.footer}>
+                <Pagination total={13} page={Number(state.page) + 1} onChange={onPageChange} />
+            </div>  
         </div>
     )
 
