361214cca1fd1d7ef663f3906641de77aa21ba35
forzys <zys@outlook.in>
2023-07-06 22:33:37 +0800

    update calender style

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,16 +1,9 @@
  
-import { memo, useCallback, useLayoutEffect } from "react"; 
-import Input from '@/components/input'
-import Button from '@/components/button'
-import { Search } from '@/components/icons' 
-import Stretch from '@/components/stretch' 
- 
-import { useMemoizedFn, useFullScreen, useCreation, usePagination, useUpdate } from '@/common/hooks' 
-import { useFetch, apis } from '@/request/index'
-import Group from "@/components/group";
-import Tooltip from '@/components/tooltip'
-import Card from "@/components/card";
-import './index.css'
+import { memo,  useLayoutEffect } from "react";  
+import {   useUpdate } from '@/common/hooks' 
+import { useFetch, apis } from '@/request/index' 
+// import Card from "@/components/card";
+import styles from './index.module.css'
 
 
 
@@ -19,8 +12,7 @@ export default memo((props)=>{
     const [state, setState ] = useUpdate({ loading: true })
 
   
-    useLayoutEffect(()=>{
-      
+    useLayoutEffect(()=>{ 
         const holiday = String(apis.holiday).replace('$var', '2023');
 
         https?.get(holiday).then((res: any)=>{
@@ -37,36 +29,39 @@ export default memo((props)=>{
         }); 
 
     },[])
+
+
+
+
     return (
-        <div className="main"> 
-           <Card style={{paddingBottom:0}}> 
-                <div className="holiday-header" style={{ paddingBottom: 12 }}> 
+        <div className="main" style={{background: 'rgba(0,0,0,0)'}}>  
+
+           <div className={styles.holiday}>
+                <header className={styles.holidayHeader} style={{ paddingBottom: 12 }}> 
                     <span style={{fontSize: 16}}>放假安排</span>
                     <div style={{ fontSize: 12}}>国务院公布的全年法定节假日安排</div> 
-                </div>
+                </header>
                 
-                <Card className="holiday-body" style={{ paddingTop: 0 }} bodyStyle={{padding: 12}}> 
-                    <div>
-                        {
-                            state?.holiday?.map((name: string)=>{
-                                const current = state?.days[name] 
-                                const length = current?.length
-                                const first = current[0]
-                                const last =  current[length - 1]
-    
-                                return (
-                                    <p className="holiday-row" key={first+last}>
-                                        <span style={{flex: 1, fontWeight:600}}>{name}</span>
-                                        <span style={{flex: 2 }}>{first} - {last}</span>
-                                        <span style={{width: 48, textAlign:'center'}}>{length}</span>
-                                    </p>
-                                )
-                            })
-                        }
-                    </div>
-                </Card> 
-            </Card>
-        
+                <div className={styles.holidayBody}>
+                    {
+                        state?.holiday?.map((name: string)=>{
+                            const current = state?.days[name] 
+                            const length = current?.length
+                            const first = current[0]
+                            const last =  current[length - 1]
+
+                            return (
+                                <p className={styles.holidayRow} key={first+last}>
+                                    <span style={{flex: 1, fontWeight:600}}>{name}</span>
+                                    <span style={{flex: 2 }}>{first} - {last}</span>
+                                    <span style={{width: 48, textAlign:'center'}}>{length}</span>
+                                </p>
+                            )
+                        })
+                    }
+                </div>
+            
+            </div> 
         </div>
     )
 })
