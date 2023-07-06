3a46726f37cf46c2a81755725ee628b6cf2e67e6
forzys <zys@outlook.in>
2023-07-07 01:22:26 +0800

    update Layout style

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -11,6 +11,8 @@ import styles from './index.module.css';
 
 
 type SegmentType = {
+    title?: string | number
+    key?: string | number
     label?: string | number
     value?: string | number
 }
@@ -26,11 +28,50 @@ export default memo((props: any)=>{
    
     const init = useMemo(()=>location?.pathname?.replace('/', ''),[])
 
+
+    const onChange = (item: SegmentType)=>{
+ 
+        navigate('/' + item.key) 
+    }
+ 
+
     return (
-        <>
+        <> 
    
          <header className={styles.header}> 
-            <div className={styles.inner}>
+            <div className={styles.outer}>
+                <div className={styles.inner}>
+                    {
+                        [
+                            { title: 'Blog', key: 'blog' }, 
+                            { title: 'Component', key: 'component'  }, 
+                            { title: 'Theme',  key: 'theme' }
+                        ].map((item) => {
+                            return (
+                                <div className={styles.item} key={item.key} onClick={onChange.bind(null,item)}>
+                                    {item.title}
+                                </div>
+                            )
+                        })
+                    } 
+                </div>
+                <div className={styles.inner}>
+                    {
+                         [
+                            { title: 'Plugins', key: 'plugins' },  
+                        ].map((item) => {
+                            return (
+                                <div className={styles.item} key={item.key} onClick={onChange.bind(null,item)}>
+                                    {item.title}
+                                </div>
+                            )
+                        })
+                    } 
+                </div>
+            </div>
+                
+          
+            {/* <div className={styles.inner}>
                 <Segment 
                     fontSize="0.8rem"
                     init={init}
@@ -43,8 +84,7 @@ export default memo((props: any)=>{
                     ]}
                     onChange={onSegmentChange}
                 />
-            </div>
-           
+            </div> */}
         </header> 
         
         <main className={styles.main}>
